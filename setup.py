9c18239b331b82ed26f4e7fdcc0c75ebf1a282b6
Matthew Walker <mattgwwalker@gmail.com>
2020-07-31 16:03:08 +1200

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='singtcommon',
-      version='0.5',
+      version='0.6.0',
       packages=["singtcommon"],
       include_package_data=True,
       install_requires=[
